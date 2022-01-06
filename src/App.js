3e96d8210e601f266b62490bfaf83e03a4b7ce86
dongji <dongji20@mails.ucas.ac.cn>
2022-01-06 10:39:20 +0800

    update route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import { Button, message } from 'antd'
+//import { Button, message } from 'antd'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/login/login';
+import Admin from './pages/admin/admin';
 
 // 应用根组件
 
 //创建类组件 并暴露出去
-export default class App extends React.Component{//react所有组件名首字母必须大写。component是react的属性。
-
-    handleClick = () => {
-        message.success('成功啦');
-    }
+export default class App extends React.Component{//react所有组件名首字母必须大写，如App。   component是react的属性。
     render(){
         return (
-            <Button type="primary" onClick={this.handleClick}>antd</Button>
+            <BrowserRouter>
+              <Switch>{/*只匹配其中一个*/}
+                  <Route path='/login' component={Login}></Route>
+                  <Route path='/' component={Admin}></Route>
+              </Switch>
+            </BrowserRouter>
         )  
     }
 }
