e69f532421eabe2c45f91bb409a4cbb795d95c72
dongji <dongji20@mails.ucas.ac.cn>
2022-01-08 20:25:39 +0800

    submit something about login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -11,8 +11,23 @@ import logo from './images/logo.png'//引入图片。
 
 const Item = Form.Item; 
 
-export default class Login extends Component{
+class Login extends Component{
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('校验成功');
+            } else {
+                console.log('校验失败');
+            }
+        })
+    }
+
     render () {
+        const form = this.props.form;
+        const {getFieldDecorator} = form;
         return (
             <div className="login">
                 <header className="login-header">
@@ -22,18 +37,41 @@ export default class Login extends Component{
                 <section className="login-content">
                     <h2>用户登陆</h2>
                     <Form onSubmit={this.handleSubmit} className="login-form">
-                        <Item>
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} //prefix是带有前缀图标的input。它的值可以是ReactNode即标签结构。Icon是图标，图标的类型是user。
-                                placeholder="用户名"
-                            />
+                        <Item>{/* 用户名input框。 */}
+                            {
+                            getFieldDecorator('username', {
+                                rules: [
+                                    {required: true, message: '用户名必须输入'},
+                                    {min: 4, message: '用户名至少4位'},
+                                    {max: 12, message: '用户名最多12位'},
+                                    {pattern: /^[A-z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成'}
+                                ],
+                                initialValue:'admin'
+                                })(
+                                <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} //prefix是带有前缀图标的input。它的值可以是ReactNode即标签结构。Icon是图标，图标的类型是user。
+                                    placeholder="用户名"
+                                />
+                                )                               
+                            }
                         </Item>
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="密码"
-                            />
+                        <Form.Item>{/* 密码input框。 */}
+                            {
+                            getFieldDecorator('password',{
+                                rules: [
+                                    {required: true, message: '密码必须输入'},
+                                    {min: 4, message: '密码至少4位'},
+                                    {max: 12, message: '密码最多12位'},
+                                    {pattern: /^[A-z0-9_]+$/, message: '密码必须是英文、数字或下划线组成'}
+                                ]
+                            })(
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="密码"
+                                />
+                                )                               
+                            }                           
                         </Form.Item>
                         <Form.Item>
                             <Button type="primary" htmlType="submit" className="login-form-button"> {/* 按钮分为普通按钮和提交按钮。htmlType="submit"表示提交按钮。 */}
@@ -46,3 +84,5 @@ export default class Login extends Component{
         )
     }
 }
+const WrapLogin = Form.create()(Login);
+export default WrapLogin;
